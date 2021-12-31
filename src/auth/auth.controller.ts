e54bd5d890beaf7639472a99358f43c6ccd8fa2c
SarathAR <sarathradhakrishnan@qburst.com>
2021-12-31 23:54:26 +0530

    swagger setup complete

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,20 @@
 import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
+import { ApiBody, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { JwtAuthGuard } from 'src/guards/jwt-auth.guard';
 import { LocalAuthGuard } from 'src/guards/local-auth.guard';
 import { AuthService } from './auth.service';
+import { LoginInput, LoginInputDto, LoginOutput } from './dto/auth.dto';
 
 @Controller('auth')
+@ApiTags('Authentication')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('login')
   @UseGuards(LocalAuthGuard)
-  async userLogin(@Request() req) {
+  @ApiBody({ type: LoginInput })
+  @ApiResponse({ type: LoginOutput })
+  async userLogin(@Request() req: LoginInputDto): Promise<LoginOutput> {
     return this.authService.userLogin(req.user);
   }
 
