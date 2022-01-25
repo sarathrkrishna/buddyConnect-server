2cb0734e94cd05dc077ec1e5b976654ebb453853
SarathAR <sarathradhakrishnan@qburst.com>
2022-01-25 12:07:49 +0530

    added soft-delete is_deleted parameter check to search and get-all queries - chat

--- src/chat/queries/chat.queries.ts
+++ src/chat/queries/chat.queries.ts
@@ -33,44 +33,35 @@ export const createChatQuery = `
 
 export const searchChatQuery = `
 	WITH
-	user_ids_from_search AS (
-		SELECT id, username, full_name, display_picture_url FROM client_master 
-		WHERE (username ILIKE $1 OR full_name ILIKE $1) 
-		AND NOT is_disabled AND id <> $2
--- 		if the user is searching his name, no result should appear
+	chats_of_user AS (
+		SELECT chat_id FROM chat_master WHERE client_id=$2 AND NOT is_deleted
 	),
-	chats_by_client_ids AS (
-		SELECT uifs.username, uifs.full_name, cm.client_id, cm.chat_id, display_picture_url FROM user_ids_from_search uifs 
-		INNER JOIN chat_master cm ON uifs.id = cm.client_id
+	user_chat_data_by_chat_id AS (
+		SELECT cm.chat_id, cm.client_id, username, full_name, display_picture_url, last_updated FROM chat_data cd 
+		INNER JOIN chat_master cm ON cd.id=cm.chat_id
+		INNER JOIN client_master clm ON cm.client_id=clm.id
+		WHERE cd.id IN (SELECT chat_id FROM chats_of_user) AND NOT cd.is_group AND cm.client_id <> $2
+ 		AND (clm.username ILIKE $1 OR clm.full_name ILIKE $1)
 	),
-  	this_client_included_chats AS (
- 		SELECT username, full_name, cbci.client_id, cbci.chat_id, display_picture_url FROM chats_by_client_ids cbci INNER JOIN chat_master cm ON cbci.chat_id=cm.chat_id 
-		WHERE cm.client_id = $2
- 	),
-	chat_infos AS (
-		SELECT client_id, chat_id, username, full_name, display_picture_url, last_updated FROM this_client_included_chats tcic 
-		INNER JOIN chat_data cd ON tcic.chat_id=cd.id WHERE NOT is_group
-	),
-	total_results_count AS (
-		SELECT COUNT(*) AS total_results_count FROM chat_infos
+	total_count AS (
+		SELECT COUNT (*) AS total_results_count FROM user_chat_data_by_chat_id
 	)
-SELECT *, (SELECT * FROM total_results_count) FROM chat_infos ORDER BY last_updated DESC LIMIT $3 OFFSET $4 
+	SELECT *, (SELECT * FROM total_count) FROM user_chat_data_by_chat_id ORDER BY last_updated DESC LIMIT $3 OFFSET $4
 `;
 
 export const getAllChatsQuery = `
-	WITH 
-	client_non_group_chats AS (
-		SELECT cm.chat_id FROM chat_master cm INNER JOIN chat_data cd ON cm.chat_id = cd.id
-		WHERE cm.client_id = $1 AND NOT cd.is_group
+	WITH
+	chats_of_user AS (
+		SELECT chat_id FROM chat_master WHERE client_id=$1 AND NOT is_deleted
 	),
-	chat_and_member_info AS (
-		SELECT cm.client_id, cm.chat_id, username, full_name, display_picture_url, cd.last_updated FROM chat_master cm 
-		INNER JOIN client_master clm ON cm.client_id = clm.id
-		INNER JOIN chat_data cd ON cm.chat_id = cd.id
-		WHERE cm.chat_id IN (SELECT chat_id FROM client_non_group_chats) AND cm.client_id <> $1
+	user_chat_data_by_chat_id AS (
+		SELECT cm.chat_id, cm.client_id, username, full_name, display_picture_url, last_updated FROM chat_data cd 
+		INNER JOIN chat_master cm ON cd.id=cm.chat_id
+		INNER JOIN client_master clm ON cm.client_id=clm.id
+		WHERE cd.id IN (SELECT chat_id FROM chats_of_user) AND NOT cd.is_group AND cm.client_id <> $1
 	),
-	total_results_count AS (
-		SELECT COUNT (*) as total_results_count FROM chat_and_member_info
+	total_count AS (
+		SELECT COUNT (*) AS total_results_count FROM user_chat_data_by_chat_id
 	)
-	SELECT *, (SELECT total_results_count FROM total_results_count) FROM chat_and_member_info ORDER BY last_updated DESC LIMIT $2 OFFSET $3;
+	SELECT *, (SELECT * FROM total_count) FROM user_chat_data_by_chat_id ORDER BY last_updated DESC LIMIT $2 OFFSET $3
 `;
