77902c1be8289bc49428b1d1cc2715a96a9d34a5
SarathAR <sarathradhakrishnan@qburst.com>
2022-01-28 17:24:16 +0530

    added swagger annotations

--- src/message/message.controller.ts
+++ src/message/message.controller.ts
@@ -6,23 +6,26 @@ import {
   Request,
   Param,
 } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiParam, ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { DatabaseService } from 'src/database/database.service';
 import { JwtAuthGuard } from 'src/guards/auth/jwt-auth.guard';
 import { GeneralRequestDto } from 'src/shared/dtos/auth/autherization.user.dto';
-import { CreateMessageInputDto } from './dto/message.dto';
+import {
+  CreateMessageInputDto,
+  CreateMessageQueryOutputDto,
+} from './dto/message.dto';
 import { MessageService } from './message.service';
 
 @Controller('message')
 @ApiTags('Message-APIs')
 export class MessageController {
-  constructor(
-    private readonly databaseService: DatabaseService,
-    private readonly messagingService: MessageService,
-  ) {}
+  constructor(private readonly messagingService: MessageService) {}
 
   @Post('create/:chat_id')
   @UseGuards(JwtAuthGuard)
+  @ApiParam({ name: 'chat_id', required: true, type: String })
+  @ApiQuery({ type: CreateMessageInputDto })
+  @ApiResponse({ type: CreateMessageQueryOutputDto })
   async createMessage(
     @Body() body: CreateMessageInputDto,
     @Request() req: GeneralRequestDto,
